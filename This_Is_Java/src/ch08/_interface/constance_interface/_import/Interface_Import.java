373653194a8da215e8c5d26b24ea889fe61e0f25
heoyun <heuyoon19@gmail.com>
2025-01-03 14:33:18 +0900

    java

--- This_Is_Java/src/ch08/_interface/constance_interface/_import/Interface_Import.java
+++ This_Is_Java/src/ch08/_interface/constance_interface/_import/Interface_Import.java
@@ -3,6 +3,7 @@ import ch08._interface.constance_interface.Remotecontrol;
 
 public class Interface_Import {
 	public static void main(String[] args) {
+		
 		/*
 		 * 인터페이스도 다른 패키지에서 사용기 위해서는 import를 시켜줘야 한다.
 		 */
